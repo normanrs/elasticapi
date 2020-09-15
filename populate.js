f752a8b34562c4c0a29c3fc89c59d92be9394744
Norm Schultz <schultzn@aetna.com>
2020-09-15 09:12:32 -0600

    Update populate.js
    Trying to address async issues

--- populate.js
+++ populate.js
@@ -3,24 +3,28 @@ const csv = require('csv-parser');
 const fs = require('fs');
 var results = [];
 
-function writeEs(object) {
-  esClient.putScript({
-    id: 'zipcode_latlong',
-    body: object
-  });
-}
-
-function getZips() {
-  fs.createReadStream('zipcode_latlngs.csv')
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-        console.log(`Writing ${results.length} records:`);
-        results.slice(0,10).forEach(element => {
-          console.log(element);
-          writeEs(element);
+const getZipData = function () {
+    return new Promise(function (resolve) {
+      fs.createReadStream('zipcode_latlngs.csv')
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+          console.log(`Stored ${results.length} records:`);
         });
     });
 };
 
-getZips();
+const writeToEs = function () {
+    return new Promise(function (resolve) {
+        console.log(`${results.length} records available`);
+        esClient.putScript({
+          id: 'zipcode_latlong',
+          body: results[0]
+        });
+    });
+};
+
+
+getZipData().then(function() {
+  return writeToEs();
+});
