4d70bd0d00d5de9e54cd8b60dd80170c9142017d
Norm Schultz <schultzn@aetna.com>
2020-09-21 15:33:32 -0600

    Update search.spec.js

--- test/search.spec.js
+++ test/search.spec.js
@@ -1,20 +1,17 @@
 const search = require('../search.js');
 
-describe("returnDate", () => {
-    it("should return a date value", () => {
-        expect.anything(search.returnDate());
-    });
-});
-
 describe("getCount()", () => {
     it("should return record", () => {
-        expect.anything(search.getCount());
+      (async () => {
+        const result = await search.getCount()
+        expect(result).contains('100')
+      })()
     });
 });
 
 
 describe("LamontData()", () => {
-    it("should return record", () => {
+    it("should return Lamont", () => {
       (async () => {
         const city = await search.getLamont().hits.hits[0]._source.City
         expect(city).equals('Lamont')
